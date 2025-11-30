e9764b148c5dbd1e9c92ada8826d6af73860b1ca
dalaohuuu <abc@a.a>
2025-11-30 18:53:18 +0800

    增加时区、swap、主机名

--- ubuntu-storage-early.sh
+++ ubuntu-storage-early.sh
@@ -2,14 +2,13 @@
 sed -i -E '/^\.{3}$/d' /autoinstall.yaml
 echo 'storage:' >>/autoinstall.yaml
 
-# 禁用 swap
+# 禁用 swap（由 extra_cache_size 控制）
 cat <<EOF >>/autoinstall.yaml
   swap:
     size: 0
 EOF
 
-# 是用 size 寻找分区，number 没什么用
-# https://curtin.readthedocs.io/en/latest/topics/storage.html
+# 计算 OS 分区大小
 size_os=$(lsblk -bn -o SIZE /dev/disk/by-label/os)
 
 # shellcheck disable=SC2154
@@ -19,13 +18,12 @@ if parted "/dev/$xda" print | grep '^Partition Table' | grep gpt; then
         size_efi=$(lsblk -bn -o SIZE /dev/disk/by-label/efi)
         cat <<EOF >>/autoinstall.yaml
   config:
-    # disk
     - ptable: gpt
       path: /dev/$xda
       preserve: true
       type: disk
       id: disk-xda
-    # efi 分区
+
     - device: disk-xda
       size: $size_efi
       number: 1
@@ -37,7 +35,7 @@ if parted "/dev/$xda" print | grep '^Partition Table' | grep gpt; then
       volume: partition-efi
       type: format
       id: format-efi
-    # os 分区
+
     - device: disk-xda
       size: $size_os
       number: 2
@@ -48,7 +46,7 @@ if parted "/dev/$xda" print | grep '^Partition Table' | grep gpt; then
       volume: partition-os
       type: format
       id: format-os
-    # mount
+
     - path: /
       device: format-os
       type: mount
@@ -59,25 +57,23 @@ if parted "/dev/$xda" print | grep '^Partition Table' | grep gpt; then
       id: mount-efi
 EOF
     else
-        # bios > 2t
         size_biosboot=$(parted "/dev/$xda" unit b print | grep bios_grub | awk '{print $4}' | sed 's/B$//')
         cat <<EOF >>/autoinstall.yaml
   config:
-    # disk
     - ptable: gpt
       path: /dev/$xda
       preserve: true
       grub_device: true
       type: disk
       id: disk-xda
-    # biosboot 分区
+
     - device: disk-xda
       size: $size_biosboot
       number: 1
       preserve: true
       type: partition
       id: partition-biosboot
-    # os 分区
+
     - device: disk-xda
       size: $size_os
       number: 2
@@ -88,7 +84,7 @@ EOF
       volume: partition-os
       type: format
       id: format-os
-    # mount
+
     - path: /
       device: format-os
       type: mount
@@ -96,17 +92,15 @@ EOF
 EOF
     fi
 else
-    # bios
     cat <<EOF >>/autoinstall.yaml
   config:
-    # disk
     - ptable: msdos
       path: /dev/$xda
       preserve: true
       grub_device: true
       type: disk
       id: disk-xda
-    # os 分区
+
     - device: disk-xda
       size: $size_os
       number: 1
@@ -117,11 +111,13 @@ else
       volume: partition-os
       type: format
       id: format-os
-    # mount
+
     - path: /
       device: format-os
       type: mount
       id: mount-os
 EOF
 fi
-echo ... >>/autoinstall.yaml
+
+# 如果 extra_cache_size 存在，autoinstall.yaml 会被追加 swap:size=XXM
+echo "..." >>/autoinstall.yaml
